714c81d731b0b36c753a40f96e296fd7483107bb
jadey-401 <81697549+jadey-401@users.noreply.github.com>
2022-03-27 21:25:24 -0500

    Add files via upload

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+	$("#logo").click(function(){
+		$("#nav-home-tab").click();
+	});
 	$("#divProducts .card").click(function(){
 		var src = $(this).find('img').attr('src');
 		var title = $(this).find('div').find('p').text();
